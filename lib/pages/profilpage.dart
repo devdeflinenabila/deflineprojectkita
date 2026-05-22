375b0d15035f072aa3750a7db148c9c04459c7a9
fadli <rosulihammadrosuli@gmail.com>
2026-05-22 09:06:19 +0700

    penambahan profilpage

--- lib/pages/profilpage.dart
+++ lib/pages/profilpage.dart
@@ -10,6 +10,38 @@ class Profilpage extends StatefulWidget {
 class _ProfilpageState extends State<Profilpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("profil"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.cyan, Colors.purple, Colors.pink],
+            ),
+          ),
+        ),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: Image.asset(
+                'aset/image/kisah2.jpg',
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              "you kisah is your images",
+              style: TextStyle(color: Colors.black, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
